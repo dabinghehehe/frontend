8ae4969133e667d36137b5dfd070471d86e4108c
dabinghehehe <956574869@qq.com>
2022-06-16 18:14:56 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//React:框架核心包
+//ReactDom:专门做渲染相关的包
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//应用的全局样式文件
+import './index.css';
+//引入根组件
+import App from './App';
+
+//渲染根组件APP 到一个id为root的dom节点上  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
